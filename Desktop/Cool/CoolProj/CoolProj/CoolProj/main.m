43ea7c6dba8e5eb138c5d0b023665a1ebd48e0b7
aimsatarana2 <aimsatarana2@gmail.com>
2013-11-27 15:17:35 +0700

    first commit

---
+++ Desktop/Cool/CoolProj/CoolProj/CoolProj/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoolProj
+//
+//  Created by iMac on 11/27/13.
+//  Copyright (c) 2013 iMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
